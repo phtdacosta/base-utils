4cf7123f33c8fd1a6459fec63212005742d8126d
phtdacosta <pedrohenriqueteodorodacosta@gmail.com>
2018-05-20 02:56:17 -0300

    Misspelled include guard fixed and function names improved

--- baseutils.h
+++ baseutils.h
@@ -1,15 +1,18 @@
-#ifndef SERIALLIB_H
-#define SERIALLIB_H
+#ifndef BASEUTILS_H
+#define BASEUTILS_H
+
+#include <stdint.h>
 
 // Those specialized functions were written to simplify when converting a hash function output into an actual number for any purpose
-unsigned long long int uint36to10_ (const char* str_);
-signed long long int sint36to10_ (const char* str_);
-unsigned long long int uint62to10_ (const char* str_);
-signed long long int sint62to10_ (const char* str_);
-// The standard atoi function is more than 2x slower than this implementation
-unsigned long long int atoi_ ( const char* str_ );
-// Those functions greatly improves the use of the library, allowing developers to shift between bases using a custom charset
-char* basetobase (const char* str_, unsigned char a_, unsigned char b_);
-char* basetobase_cc (const char* str_, unsigned char a_, unsigned char b_, const char* charset_);
+uint64_t uconvb36tob10 (const char* str);
+int64_t convb36tob10 (const char* str);
+uint64_t uconvb62tob10 (const char* str);
+int64_t convb62tob10 (const char* str);
+// This implementation is (more than) twice as faster than the standard function, but only handles unsigned values
+uint64_t uatoi (const char* str);
+// Those functions greatly improves the use of the library,
+// allowing developers to shift between bases (2 up to 62) and using a custom charset within ASCII table (0-255)
+char* basetobase (const char* str, uint8_t a, uint8_t b);
+char* basetobasecustom (const char* str, uint8_t a, uint8_t b, const char* charset);
 
 #endif
