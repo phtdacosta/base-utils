4c6bf53225d2794f87bd1071821fff73634b75f4
phtdacosta <pedrohenriqueteodorodacosta@gmail.com>
2018-05-20 02:58:04 -0300

    Source code updated accordingly to header file

--- baseutils.c
+++ baseutils.c
@@ -5,15 +5,15 @@
 #include <stdbool.h>
 #include "baseutils.h"
 
-const char _94[] = {
-    ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    ':', ';', '<', '=', '>', '?', '@',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z',
-    '[', '\\', ']', '^', '_', '`',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z',
-    '{', '|', '}', '~'
-    };
+// const char _94[] = {
+//     ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
+//     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+//     ':', ';', '<', '=', '>', '?', '@',
+//     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z',
+//     '[', '\\', ']', '^', '_', '`',
+//     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z',
+//     '{', '|', '}', '~'
+//     };
 
 const char _62[] = {
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -21,183 +21,183 @@ const char _62[] = {
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     };
 
-unsigned long long int uint36to10_ (const char* str_) {
-    unsigned long long int aux = 0;
-    unsigned char aux_ = strlen(str_);
-    while ( *str_ ) {
-        ((*str_ - 0) > 64) ? 
-            (aux += (9 + ((*str_++ - 0) - 64)) * pow(36, --aux_)) :
-            (aux += ((*str_++ - 0) - 48) * pow(36, --aux_)); 
+uint64_t uconvb36tob10 (const char* str) {
+    uint64_t dec = 0;
+    uint8_t len = strlen(str);
+    while (*str) {
+        ((*str - 0) > 64) ? 
+            (dec += (9 + ((*str++ - 0) - 64)) * pow(36, --len)) :
+            (dec += ((*str++ - 0) - 48) * pow(36, --len)); 
     }
-    return aux;
+    return dec;
 }
 
-signed long long int sint36to10_ (const char* str_) {
-    signed long long int aux = 0;
-    unsigned char aux_ = strlen(str_);
-    bool _;
-    (*str_++ == '-') ? (_ = true, aux_--) : (_ = false);
-    while ( *str_ ) {
-        ((*str_ - 0) > 64) ? 
-            (aux += (9 + ((*str_++ - 0) - 64)) * pow(36, --aux_)) :
-            (aux += ((*str_++ - 0) - 48) * pow(36, --aux_)); 
+int64_t convb36tob10 (const char* str) {
+    int64_t dec = 0;
+    uint8_t len = strlen(str);
+    bool neg;
+    (*str++ == '-') ? (neg = true, len--) : (neg = false);
+    while (*str) {
+        ((*str - 0) > 64) ? 
+            (dec += (9 + ((*str++ - 0) - 64)) * pow(36, --len)) :
+            (dec += ((*str++ - 0) - 48) * pow(36, --len)); 
     }
-    return (_) ? (aux - aux - aux) : (aux);
+    return (neg) ? (dec - dec - dec) : (dec);
 }
 
-unsigned long long int uint62to10_ (const char* str_) {
-    unsigned long long int aux = 0;
-    unsigned char aux_ = strlen(str_);
-    while ( *str_ ) {
-        ((*str_ - 0) > 96) ? 
-            (aux += (9 + ((*str_++ - 0) - (96 - 26))) * pow(62, --aux_)) :
-            ( ((*str_ - 0) > 64) ? (aux += (9 + ((*str_++ - 0) - 64)) * pow(62, --aux_)) : (aux += ((*str_++ - 0) - 48) * pow(62, --aux_)) ); 
+uint64_t uconvb62tob10 (const char* str) {
+    uint64_t dec = 0;
+    uint8_t len = strlen(str);
+    while (*str) {
+        ((*str - 0) > 96) ? 
+            (dec += (9 + ((*str++ - 0) - (96 - 26))) * pow(62, --len)) :
+            ( ((*str - 0) > 64) ? (dec += (9 + ((*str++ - 0) - 64)) * pow(62, --len)) : (dec += ((*str++ - 0) - 48) * pow(62, --len)) ); 
     }
-    return aux;
+    return dec;
 }
 
-signed long long int sint62to10_ (const char* str_) {
-    signed long long int aux = 0;
-    unsigned char aux_ = strlen(str_);
-    bool _;
-    (*str_++ == '-') ? (_ = true, aux_--) : (_ = false);
-    while ( *str_ ) {
-        ((*str_ - 0) > 96) ? 
-            (aux += (9 + ((*str_++ - 0) - (96 - 26))) * pow(62, --aux_)) :
-            ( ((*str_ - 0) > 64) ? (aux += (9 + ((*str_++ - 0) - 64)) * pow(62, --aux_)) : (aux += ((*str_++ - 0) - 48) * pow(62, --aux_)) ); 
+int64_t convb62tob10 (const char* str) {
+    int64_t dec = 0;
+    uint8_t len = strlen(str);
+    bool neg;
+    (*str++ == '-') ? (neg = true, len--) : (neg = false);
+    while (*str) {
+        ((*str - 0) > 96) ? 
+            (dec += (9 + ((*str++ - 0) - (96 - 26))) * pow(62, --len)) :
+            ( ((*str - 0) > 64) ? (dec += (9 + ((*str++ - 0) - 64)) * pow(62, --len)) : (dec += ((*str++ - 0) - 48) * pow(62, --len)) ); 
     }
-    return (_) ? (aux - aux - aux) : (aux);
+    return (neg) ? (dec - dec - dec) : (dec);
 }
 
-unsigned long long int atoi_ ( const char* str_ ) {
-    unsigned long long int val_ = 0;
-    while ( *str_ ) {
-        val_ = val_ * 10 + (*str_++ - '0');
+uint64_t uatoi (const char* str) {
+    uint64_t val = 0;
+    while (*str) {
+        val = val * 10 + (*str++ - '0');
     }
-    return val_;
+    return val;
 }
 
-static inline signed short int locatecharsetval_ (const char* charset_, unsigned char char_) {
-    signed short int aux;
+static inline uint_fast16_t locatevalue_ (const char* charset_, uint8_t char_) {
+    uint_fast16_t ind_ ;
     while ( *charset_ ) {
         if (*charset_ == char_) {
-            return aux;
+            return ind_;
         } else {
             *charset_++;
-            aux++;
+            ind_++;
         };
     }
     return -1;
 }
 
-char* basetobase (const char* str_, unsigned char a_, unsigned char b_) {
-    char* arr_ = malloc(sizeof(char) * 50);
-    char* temp_ = arr_;
-    unsigned long long int aux = 0;
-    unsigned char aux_ = strlen(str_);
-    unsigned char aux__ = 0;
+char* basetobase (const char* str, uint8_t a, uint8_t b) {
+    char* arr = malloc(sizeof(char) * 50); // 50 to ensure a secure margin in case of huge representations
+    char* tmp = arr;
+    uint64_t aux = 0;
+    uint8_t len = strlen(str);
+    uint8_t lvl = 0;
 
-    bool neg_;
-    (*str_ == '-') ? (neg_ = true, *str_++) : (neg_ = false);
+    bool neg;
+    (*str == '-') ? (neg = true, *str++) : (neg = false);
 
     // Before enconding to the new requested base, the numeric representation is pre-encoded in decimal
-    while ( *str_ ) {
-        ((*str_ - 0) > 96) ? 
-            (aux += (9 + ((*str_++ - 0) - (96 - 26))) * pow(a_, --aux_)) :
-            ( ((*str_ - 0) > 64) ? (aux += (9 + ((*str_++ - 0) - 64)) * pow(a_, --aux_)) : (aux += ((*str_++ - 0) - 48) * pow(a_, --aux_)) ); 
+    while ( *str ) {
+        ((*str - 0) > 96) ? 
+            (aux += (9 + ((*str++ - 0) - (96 - 26))) * pow(a, --len)) :
+            ( ((*str - 0) > 64) ? (aux += (9 + ((*str++ - 0) - 64)) * pow(a, --len)) : (aux += ((*str++ - 0) - 48) * pow(a, --len)) ); 
     }
 
     // Successive divisions to find the new numeric representation
-    while (aux >= b_) {
-        ++aux__;
-        // *arr_++ = aux % b_;
+    while (aux >= b) {
+        ++lvl;
+        // *arr++ = aux % b;
         // C = A – B * (A / B)
-        *arr_++ = aux - b_ * (aux / b_);
-        aux = aux / b_;
+        *arr++ = aux - b * (aux / b);
+        aux = aux / b;
     }
-    *arr_ = aux;
-    aux__++;
+    *arr = aux;
+    lvl++;
 
     // Creates a new specialized array where the new representation will be stored and checks if the number will be negative
     // The +1 is for adding the '\0' character at the end of the function to avoid trash characters
-    char* arr__ = malloc(sizeof(char) * (aux__ + 1));
-    char* __ = arr__;
-    if (neg_) {
-        (*arr__++ = '-');
-        aux__++;
+    char* rep = malloc(sizeof(char) * (lvl + 1));
+    char* res = rep;
+    if (neg) {
+        (*rep++ = '-');
+        lvl++;
     }
 
     // Reverses the array, replacing each decimal value by the corresponding value given by a "charset lookup table"
-    while (aux__ > 0) {
-        *arr__++ = _62[temp_[--aux__]];
+    while (lvl > 0) {
+        *rep++ = _62[tmp[--lvl]];
     }
-    *arr__ = '\0';
+    *rep = '\0';
 
     // Cleans the space used to store the support variable array 
-    // if (arr_ != NULL) {
-    //     free(arr_);
-    //     arr_ = NULL;
-    // }
+    if (tmp != NULL) {
+        free(tmp);
+        tmp = NULL;
+    }
 
-    return __;
+    return res;
 }
 
-char* basetobase_cc (const char* str_, unsigned char a_, unsigned char b_, const char* charset_) {
-    char* arr_ = malloc(sizeof(char) * 50);
-    char* temp_ = arr_;
-    unsigned long long int aux = 0;
-    unsigned char aux_ = strlen(str_);
-    unsigned char aux__ = 0;
+char* basetobasecustom (const char* str, uint8_t a, uint8_t b, const char* charset) {
+    char* arr = malloc(sizeof(char) * 50); // 50 to ensure a secure margin in case of huge representations
+    char* tmp = arr;
+    uint64_t aux = 0;
+    uint8_t len = strlen(str);
+    uint8_t lvl = 0;
 
-    bool neg_;
-    (*str_ == '-') ? (neg_ = true, *str_++) : (neg_ = false);
+    bool neg;
+    (*str == '-') ? (neg = true, *str++) : (neg = false);
 
     // Before enconding to the new requested base, the numeric representation is pre-encoded in decimal
-    signed short int val_;
-    while ( *str_ ) {
-        val_ = locatecharsetval_(charset_, *str_++);
-        if (val_ > 0) {
-            aux += val_ * pow(a_, --aux_);
-            *str_++;
+    uint_fast16_t ind;
+    while ( *str ) {
+        ind = locatevalue_(charset, *str++);
+        if (ind > -1) {
+            aux += ind * pow(a, --len);
+            *str++;
         } else {
-            if (arr_ != NULL) {
-                free(arr_);
-                arr_ = NULL;
+            if (arr != NULL) {
+                free(arr);
+                arr = NULL;
             }
-            return arr_;
+            return arr;
         } 
     }
     // Successive divisions to find the new numeric representation
-    while (aux >= b_) {
-        ++aux__;
-        // *arr_++ = aux % b_;
+    while (aux >= b) {
+        ++lvl;
+        // *arr++ = aux % b;
         // C = A – B * (A / B)
-        *arr_++ = aux - b_ * (aux / b_);
-        aux = aux / b_;
+        *arr++ = aux - b * (aux / b);
+        aux = aux / b;
     }
-    *arr_ = aux;
-    aux__++;
+    *arr = aux;
+    lvl++;
 
     // Creates a new specialized array where the new representation will be stored and checks if the number will be negative
     // The +1 is for adding the '\0' character at the end of the function to avoid trash characters
-    char* arr__ = malloc(sizeof(char) * (aux__ + 1));
-    char* __ = arr__;
-    if (neg_) {
-        (*arr__++ = '-');
-        aux__++;
+    char* rep = malloc(sizeof(char) * (lvl + 1));
+    char* res = rep;
+    if (neg) {
+        (*rep++ = '-');
+        lvl++;
     }
 
     // Reverses the array, replacing each decimal value by the corresponding value given by a "charset lookup table"
-    while (aux__ > 0) {
-        *arr__++ = charset_[temp_[--aux__]];
+    while (lvl > 0) {
+        *rep++ = charset[tmp[--lvl]];
     }
-    *arr__ = '\0';
+    *rep = '\0';
 
     // Cleans the space used to store the support variable array 
-    // if (arr_ != NULL) {
-    //     free(arr_);
-    //     arr_ = NULL;
-    // }
+    if (tmp != NULL) {
+        free(tmp);
+        tmp = NULL;
+    }
 
-    return __;
+    return res;
 }
